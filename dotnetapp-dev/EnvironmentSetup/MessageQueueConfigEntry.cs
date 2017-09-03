52ce5c80200a8d7de404cb1266c6e8059c8fa7c3
rogera.win10 <rogera+win10@wolfspool.at>
2017-09-03 22:11:54 +0200

    rsu green

--- dotnetapp-dev/EnvironmentSetup/MessageQueueConfigEntry.cs
+++ dotnetapp-dev/EnvironmentSetup/MessageQueueConfigEntry.cs
@@ -1,25 +1,57 @@
-﻿namespace DotnetApp.EnvironmentSetup
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MessageQueueConfigEntry.cs" company="ase">
+//   mit
+// </copyright>
+// <summary>
+//   The message queue config entry.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DotnetApp.EnvironmentSetup
 {
+    #region
+
     using DotnetApp.AseFramework.Definitions;
 
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
 
+    #endregion
+
+    /// <summary>
+    ///     The message queue config entry.
+    /// </summary>
     public class MessageQueueConfigEntry : IConfigEntry, AseFrameworkDefinitionsHead
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageQueueConfigEntry"/> class.
+        /// </summary>
+        /// <param name="hostname">
+        /// The hostname.
+        /// </param>
         public MessageQueueConfigEntry(string hostname)
         {
             this.Hostname = hostname;
             this.Purpose = ProgramConfigKeys.MessageQueue;
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MessageQueueConfigEntry" /> class.
+        /// </summary>
         public MessageQueueConfigEntry()
         {
             // todo: deprecated, use c;tor version
         }
 
+        /// <summary>
+        ///     Gets or sets the hostname.
+        /// </summary>
+        public string Hostname { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the purpose.
+        /// </summary>
         [JsonConverter(typeof(StringEnumConverter))]
         public ProgramConfigKeys Purpose { get; set; }
-        public string Hostname { get; set; }
     }
 }
