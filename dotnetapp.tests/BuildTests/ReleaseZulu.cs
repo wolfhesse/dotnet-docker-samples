18c41343b02be214cff579af2720e60e27560cc3
rogera.win10 <rogera+win10@wolfspool.at>
2017-09-22 15:29:41 +0200

    adding mstest for build scenario

---
+++ dotnetapp.tests/BuildTests/ReleaseZulu.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReleaseZulu.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The release zulu.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DotnetApp.Tests.BuildTests
+{
+    using DnsLib;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>The release zulu.</summary>
+    [TestClass]
+    public class ReleaseZulu
+    {
+        /// <summary>The t c 1.</summary>
+        [TestMethod]
+        public void TC1()
+        {
+            var version = VersionInfo.Version;
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(version);
+        }
+    }
+}
